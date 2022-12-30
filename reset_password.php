45cb183eebb9f9b591d1f07a9ae3edc3c27898b7
Ode Ardika <dirfanardika@gmail.com>
2022-12-31 06:44:37 +0800

    Add CSS to reset_password.php

--- reset_password.php
+++ reset_password.php
@@ -25,19 +25,32 @@ if (isset($_POST["kirim"])) {
 </head>
 <body>
 <form method="post">
-    <?php if (isset($error)) { ?>
     <div class="container">
-        <p>Konfirmasi password tidak valid</p>
+        <img name="login_logo"src="logo.jpg" alt="Logo" width="100">
+        <h5 name="lupa_pass_title">Lupa Password</h5>
     </div>
-    <?php } ?>
+    <div class="container">
+        <p name="silahkan_masukan_email">"Silakan masukkan password yang baru"</p>
+    </div>
     <div class="container">
         <input type="password" name="password" id="password" placeholder="Password" required>
         <input type="password" name="konfirmasi" id="konfirmasi" placeholder="Konfirmasi password" required>
     </div>
+    <?php if (isset($error)) { ?>
+    <div class="container">
+        <p name ="error">Konfirmasi password tidak valid</p>
+        <style>
+            #password,#konfirmasi{
+                border: 5px solid red;
+            }
+        </style>
+    </div>
+    <?php } ?>
     <div class="container">
         <button type="submit" name="kirim" id="kirim">Kirim</button>
     </div>
 </form>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="Style.css">
 </body>
 </html>
