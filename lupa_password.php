8b66482f0d1478bd45cb8ad582224f912c50713d
Ode Ardika <dirfanardika@gmail.com>
2022-12-31 05:53:55 +0800

    Add CSS to lupa_password.php

--- lupa_password.php
+++ lupa_password.php
@@ -26,25 +26,27 @@ if (isset($_POST["kirim"])) {
 <body>
 <form method="post">
     <div class="container">
-        <img src="logo.jpg" alt="Logo" width="100">
-        <h5>Lupa Password</h5>
+        <img name="login_logo"src="logo.jpg" alt="Logo" width="100">
+        <h5 name="lupa_pass_title">Lupa Password</h5>
     </div>
     <div class="container">
-        <p>"Silakan masukkan alamat email yang ingin dikirim stel ulang password"</p>
+        <p name="silahkan_masukan_email">"Silakan masukkan alamat email yang ingin dikirim stel ulang password"</p>
     </div>
     <?php if (isset($error)) { ?>
     <div class="container">
-        <p>Email tidak ditemukan</p>
+        <p name="email_salah">Email tidak ditemukan</p>
     </div>
     <?php } ?>
     <div class="container">
-        <input type="text" name="email" id="email" placeholder="Email" required>
+        <input type="text" name="email_lupa_pass" id="email" placeholder="Email" required>
     </div>
     <div class="container">
         <button type="submit" name="kirim" id="kirim">Kirim</button>
-        <span><a href="login.php">Back to Login</a></span>
+        <span><a name="back_login"href="login.php">Back to Login</a></span>
     </div>
 </form>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="Style.css">
+
 </body>
 </html>
